42d0f776d62e76f11123f26d5ff33dbe79374796
Jack Connor <jack.connor83@gmail.com>
2015-08-20 13:22:16 -0700

    fixed auth fixes, pushing to heroku

--- config/auth.js
+++ config/auth.js
@@ -4,19 +4,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '952644968132286', // your App ID
         'clientSecret'  : '066f229f84db627478bc75d8c4e9a91f', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'https://warm-bayou-8573.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'DQDqgK7MaCQCOg4cpTKnLMTQu',
         'consumerSecret'    : 'NPH0BTtDXoqBJIiMFVB54PGOjPDVlobFbjEr10ZtQnFSf0xyDI',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'https://warm-bayou-8573.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : 'your-secret-clientID-here',
         'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'https://warm-bayou-8573.herokuapp.com/auth/google/callback'
     }
 
 };
