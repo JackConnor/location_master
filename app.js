a453b3acbc8f80c7b1e6523773dcc803a7d28981
Jack Connor <jack.connor83@gmail.com>
2015-08-20 12:01:22 -0700

    added passport page

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var passport    = require('passport');
 var flash       = require('connect-flash');
 var morgan      = require('morgan');
 var cookieParser = require('cookie-parser');
-// var session      = require('express-session');
+var session      = require('express-session');
 var path         = require('path');
 // Let's define a schema to be used with the Book model
 
@@ -26,7 +26,7 @@ app.use(bodyParser()); // get information from html forms
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 // required for passport
-// app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages
