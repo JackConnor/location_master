5d3e21e89b2ef68f6c3eb42235cecc8e9ee0d126
Jack Connor <jack.connor83@gmail.com>
2015-08-20 12:51:49 -0700

    added route.js and pointed towards it

--- app.js
+++ app.js
@@ -64,68 +64,70 @@ app.use(function(request, response, next){
 
 app.use(bodyParser.json());
 
-var books = [
-  {id: 17, name: 'bible'}
-, {id: 21, name: "qur'an"}
-, {id: 35, name: 'torah'}
-, {id: 48, name: 'principia matematica'}
-, {id: 50, name: 'sruti'}
-, {id: 63, name: 'rastafari book'}];
+// var books = [
+//   {id: 17, name: 'bible'}
+// , {id: 21, name: "qur'an"}
+// , {id: 35, name: 'torah'}
+// , {id: 48, name: 'principia matematica'}
+// , {id: 50, name: 'sruti'}
+// , {id: 63, name: 'rastafari book'}];
+//
+// app.get('/', function(request, response){
+//   console.log(__dirname);
+//   response.sendFile(__dirname + '/index_jquery.html');
+// });
+//
+// app.get('/books', function(request, response){
+//   Book.find({}, function(err, books){
+//     if (err){
+//       console.log(err);
+//       return;
+//     }
+//     response.json(books);
+//   });
+// });
+//
+// app.post('/books', function(request, response){
+//   console.log(request.body);
+//   // Let's put a book in the database
+//   Book.create(request.body, function(err, bookThatGotSaved){
+//     if (err){
+//       // log the error so the developer understands there is an error
+//       console.log(err);
+//       // exit out this function
+//       return;
+//     }
+//     else {
+//       console.log('succesfully saved ', bookThatGotSaved);
+//       response.json(bookThatGotSaved);
+//     }
+//   });
+// });
+//
+// // Lets get an individual book by id
+// app.get('/books/:id', function(request, response){
+//   // lets find the book using mongoose
+//   Book.findOne({_id: request.params.id}, function(err, book){
+//     if (err){
+//       console.log(err);
+//       return
+//     }
+//     response.json(book);
+//   })
+// });
+//
+// app.delete('/books/:id', function(request, response){
+//   Book.remove({_id: request.params.id}, function(err, book){
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log('succesfully destroyed book with id', request.params.id);
+//     response.json({'message': 'succesfully deleted'});
+//   });
+// });
 
-app.get('/', function(request, response){
-  console.log(__dirname);
-  response.sendFile(__dirname + '/index_jquery.html');
-});
-
-app.get('/books', function(request, response){
-  Book.find({}, function(err, books){
-    if (err){
-      console.log(err);
-      return;
-    }
-    response.json(books);
-  });
-});
-
-app.post('/books', function(request, response){
-  console.log(request.body);
-  // Let's put a book in the database
-  Book.create(request.body, function(err, bookThatGotSaved){
-    if (err){
-      // log the error so the developer understands there is an error
-      console.log(err);
-      // exit out this function
-      return;
-    }
-    else {
-      console.log('succesfully saved ', bookThatGotSaved);
-      response.json(bookThatGotSaved);
-    }
-  });
-});
-
-// Lets get an individual book by id
-app.get('/books/:id', function(request, response){
-  // lets find the book using mongoose
-  Book.findOne({_id: request.params.id}, function(err, book){
-    if (err){
-      console.log(err);
-      return
-    }
-    response.json(book);
-  })
-});
-
-app.delete('/books/:id', function(request, response){
-  Book.remove({_id: request.params.id}, function(err, book){
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log('succesfully destroyed book with id', request.params.id);
-    response.json({'message': 'succesfully deleted'});
-  });
-});
+require('../easyAuth/models/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 app.listen(app.get('port'), function(){
   console.log("Server started, lets get down");
