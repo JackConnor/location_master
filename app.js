be2c0b8af7e7b2dde885d196216828b05bae1c35
Jack Connor <jack.connor83@gmail.com>
2015-08-20 11:46:01 -0700

    trying new sessions strategy

--- app.js
+++ app.js
@@ -12,9 +12,24 @@ var passport    = require('passport');
 var flash       = require('connect-flash');
 var morgan      = require('morgan');
 var cookieParser = require('cookie-parser');
-// var session      = require('express-session');
+var session      = require('express-session');
 var path         = require('path');
 // Let's define a schema to be used with the Book model
+
+require('./config/passport')(passport); // pass passport for configuration
+app.use(express.static(path.join(__dirname, 'public')));
+// set up our express application
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages
 
 app.set('port', process.env.PORT || 5000);
 var bookSchema = mongoose.Schema({
